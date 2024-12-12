d0aacf7f372f3e1e0029fb03b052e667b750b9ac
Filo-White <147394948+Filo-White@users.noreply.github.com>
2024-12-12 22:02:52 +0100

    Update advanced_rag.py

--- src/advanced_rag.py
+++ src/advanced_rag.py
@@ -33,8 +33,58 @@ from tkinter import filedialog
 # For progress tracking in loops:
 from tqdm.autonotebook import tqdm
 
-# - # - # - # - # - # - # - # - # - # - # -
+#For private info upload
+from dotenv import load_dotenv
 
+# For Llama index functionalities
+from llama_parse import LlamaParse  
 
+# =====================
+#   FILE SELECTION
+# =====================
+
+# Carica le variabili d'ambiente dal file .env
+load_dotenv()
+
+# Ottieni la chiave API di OpenAI
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+# Imposta la chiave API nell'ambiente
+if openai_api_key:
+    os.environ["OPENAI_API_KEY"] = openai_api_key
+    print("API key loaded successfully.")
+else:
+    print("API key not found in .env file.")
+
+# =====================
+#   FILE SELECTION
+# =====================
+
+def select_file():
+    root = tk.Tk()  
+    root.withdraw()  
+    
+    file_path = filedialog.askopenfilename(
+        title="Select a file",
+        filetypes=(("File OWL, PDF e TXT", "*.owl *.pdf *.txt"), ("All files", "*.*"))
+    )
+    return file_path
+
+selected_file = select_file()
+if selected_file:
+    print(f"You selected: {selected_file}")
+else:
+    print("None file selected")
+
+# =====================
+#   DOCUMENT PARSING
+# =====================
+             
+parser = LlamaParse(
+    # api_key="llx-oBMnoRgUcdtfgkyiFiDrKh5ka5IwUCqm3WlkfViw3TC8wNCA",  # chiave MARCO di https://cloud.llamaindex.ai/landing
+    api_key="llx-x01ouaHuW4QSItmZFCo0ODxNe1MVw8PCYHg2U3YTSKtKbPaS",    # chiave FILIPPO di https://cloud.llamaindex.ai/landing
+    result_type="markdown"                                             # altre opzioni: json, text, html, raw
+)
+documents = await parser.aload_data(selected_file)
 
 
