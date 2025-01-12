332eecd93fc4c0f2b457c3c62fc1cf856c658d1f
Filo-White <147394948+Filo-White@users.noreply.github.com>
2025-01-12 14:55:55 +0100

    Update martialis.py

--- src/martialis.py
+++ src/martialis.py
@@ -1,17 +1,35 @@
+from llama_index.llms.mistralai import MistralAI # %pip install llama-index-llms-mistralai
+from llama_index.core.llms import ChatMessage
+
+
 def main():
     user_prompt = input("Enter your prompt: ")
-    parsed_prompt = parse_user_prompt(user_prompt)
-    
-    if parsed_prompt == "advanced_rag":
+    response = modality_selection(user_prompt)
+
+    if "advanced_rag" in response:
         advanced_rag()
-    elif parsed_prompt == "ontology_validator":
+    elif "ontology_validator" in response:
         ontology_validator()
     else:
         print("Invalid prompt")
+        
+def modality_selection(user_prompt):
+    llm = MistralAI(model = "mistral-large-latest",
+                    api_key="") 
+    messages = [
+    ChatMessage(role="system", 
+                content="""
+                Your task is to identify the user's requested task based on their prompt. You have two options:
+                1. "advanced_rag" for question answering;
+                2. "ontology_validator" for text generation.
 
-def parse_user_prompt(prompt):
-    # Implement your parsing logic here
-    return prompt.strip().lower()
+                Analyze the user's prompt and select one of the two options: "advanced_rag" or "ontology_validator". 
+
+                You must strictly return only "advanced_rag" or "ontology_validator" without adding any additional text or context."""
+    ChatMessage(role="user", content=user_prompt),
+    ]
+    resp = llm.chat(messages)
+    return str(resp)
 
 def advanced_rag():
     print("Advanced RAG function called")
